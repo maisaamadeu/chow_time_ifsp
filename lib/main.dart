9776daa0df50c4adbb4108486e30a7202fc941d3
Maísa Amadeu <maisa.antonella.amadeu@gmail.com>
2023-05-13 19:39:43 -0300

    resolving gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:chow_time_ifsp/app_widget.dart';
+import 'package:chow_time_ifsp/shared/repositories/menus_repository.dart';
 import 'package:chow_time_ifsp/shared/services/firebase_services.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   FirebaseServices().initFirebase();
-  runApp(const AppWidget());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => MenusRepository(),
+      child: AppWidget(),
+    ),
+  );
 }
--- lib/shared/repositories/menus_repository.dart
+++ lib/shared/repositories/menus_repository.dart
@@ -1,3 +1,28 @@
+import 'package:chow_time_ifsp/shared/models/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-class MenusRepository extends ChangeNotifier {}
+class MenusRepository extends ChangeNotifier {
+  UserModel? _user;
+  QuerySnapshot? _allMenus;
+  QueryDocumentSnapshot? _currentMenu;
+
+  UserModel? get user => _user;
+  QuerySnapshot? allMenus() => _allMenus;
+  QueryDocumentSnapshot? currentMenu() => _currentMenu;
+
+  setUser(UserModel newUser) {
+    _user = newUser;
+    notifyListeners();
+  }
+
+  setAllMenus(QuerySnapshot newMenus) {
+    _allMenus = newMenus;
+    notifyListeners();
+  }
+
+  setCurrentMenu(QueryDocumentSnapshot newMenu) {
+    _currentMenu = newMenu;
+    notifyListeners();
+  }
+}
